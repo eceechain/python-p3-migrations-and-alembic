b29fb894ea3bd6ab234c99b95b8a1f9ff2b4238c
ecee <you@example.com>
2023-12-06 16:19:39 +0300

    Done

---
+++ app/migrations/versions/6ac9562982bd_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 6ac9562982bd
+Revises: 
+Create Date: 2023-12-06 12:34:49.502652
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6ac9562982bd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
